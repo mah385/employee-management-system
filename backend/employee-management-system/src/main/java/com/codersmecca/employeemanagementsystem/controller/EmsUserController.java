6c8247f497621ab321ece576e2fda98851df5058
mah385 <mdarifhussain385@gmail.com>
2024-06-23 16:11:41 +0530

    backend upgraded from JDK_17 to JDK_22

--- backend/employee-management-system/src/main/java/com/codersmecca/employeemanagementsystem/controller/EmsUserController.java
+++ backend/employee-management-system/src/main/java/com/codersmecca/employeemanagementsystem/controller/EmsUserController.java
@@ -18,40 +18,26 @@ public class EmsUserController {
 
     private final EmsUserService emsUserService;
 
-    @PostMapping(
-            value = "/add-new-ems-user",
-            consumes = {MediaType.APPLICATION_JSON_VALUE},
-            produces = {MediaType.APPLICATION_JSON_VALUE}
-    )
+    @PostMapping(value = "/add-new-ems-user", consumes = {MediaType.APPLICATION_JSON_VALUE}, produces = {MediaType.APPLICATION_JSON_VALUE})
     public ResponseEntity<EmsResponseEntity> addNewEmsUser(
             @Valid @RequestBody AddNewEmsUserRequestBean addNewEmsUserRequestBean
     ) {
         return this.emsUserService.addNewEmsUser(addNewEmsUserRequestBean);
     }
 
-    @PutMapping(
-            value = "/update-ems-user",
-            consumes = {MediaType.APPLICATION_JSON_VALUE},
-            produces = {MediaType.APPLICATION_JSON_VALUE}
-    )
+    @PutMapping(value = "/update-ems-user", consumes = {MediaType.APPLICATION_JSON_VALUE}, produces = {MediaType.APPLICATION_JSON_VALUE})
     public ResponseEntity<EmsResponseEntity> updateEmsUser(
             @Valid @RequestBody UpdateEmsUserRequestBean updateEmsUserRequestBean
     ) {
         return this.emsUserService.updateEmsUser(updateEmsUserRequestBean);
     }
 
-    @GetMapping(
-            value = "/get-all-ems-user",
-            produces = {MediaType.APPLICATION_JSON_VALUE}
-    )
+    @GetMapping(value = "/get-all-ems-user", produces = {MediaType.APPLICATION_JSON_VALUE})
     public ResponseEntity<EmsResponseEntity> getAllEmsUser() {
         return this.emsUserService.getAllEmsUser();
     }
 
-    @DeleteMapping(
-            value = "/delete-ems-user-by-id",
-            produces = {MediaType.APPLICATION_JSON_VALUE}
-    )
+    @DeleteMapping(value = "/delete-ems-user-by-id", produces = {MediaType.APPLICATION_JSON_VALUE})
     public ResponseEntity<EmsResponseEntity> deleteEmsUserById(
             @RequestParam(value = "id") String id
     ) {
